084ba6c18a76b79f6e5145d4b5d5a37902358891
SergioCaMi <sergiocami84@gmail.com>
2025-03-27 20:50:43 +0100

    fin ejercicio

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ document.querySelector('#second').addEventListener('click', updateBgColor);
 document.querySelector('#third').addEventListener('click', updateBgColor);
 
 // MODIFICAR A PARTIR DE AQUÍ 
+document.querySelector('#fourth').addEventListener('click', updateBgColor);
+const imgPicachu = document.querySelector("img");
+const colorSangría = window.getComputedStyle(document.querySelector('#fourth')).backgroundColor;
+var colorPikachu;
 
 function updateBgColor(event) {
-
+    console.log(event.target);
+    console.log(colorSangría);
+    if (event.target.getAttribute("id")=="fourth"){
+        colorPikachu=colorSangría
+    } else {
+        colorPikachu = event.target.getAttribute("data-color");
+    }
+    imgPicachu.style.backgroundColor = colorPikachu;
 }
