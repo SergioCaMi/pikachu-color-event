f5ae1cd289c9f78a1189ab65f696adce6fc73091
SergioCaMi <sergiocami84@gmail.com>
2025-03-27 20:52:40 +0100

    comentarios actualizados

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 // Pika-pika suerte!
+// Estado inicial
+const imgPicachu = document.querySelector("img");
+const colorSangría = window.getComputedStyle(document.querySelector('#fourth')).backgroundColor;
+var colorPikachu = "#000000";
 
 // NO MODIFICAR (salvo para conseguir el color sangría)
-
+//Controlar Eventos
 document.querySelector('#first').addEventListener('click', updateBgColor);
 document.querySelector('#second').addEventListener('click', updateBgColor);
 document.querySelector('#third').addEventListener('click', updateBgColor);
-
-// MODIFICAR A PARTIR DE AQUÍ 
 document.querySelector('#fourth').addEventListener('click', updateBgColor);
-const imgPicachu = document.querySelector("img");
-const colorSangría = window.getComputedStyle(document.querySelector('#fourth')).backgroundColor;
-var colorPikachu;
 
 function updateBgColor(event) {
-    console.log(event.target);
-    console.log(colorSangría);
+    //Modificar Variables
     if (event.target.getAttribute("id")=="fourth"){
         colorPikachu=colorSangría
     } else {
         colorPikachu = event.target.getAttribute("data-color");
     }
+    //Actualizar DOM
     imgPicachu.style.backgroundColor = colorPikachu;
 }
